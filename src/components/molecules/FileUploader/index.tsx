da4838340744e967f398391ef77494f420c05521
ivanDAleannlab <ivand@aleannlab.com>
2024-11-11 14:49:53 +0200

    PROJ-23 added file uploder UI and scanner UI

--- src/components/molecules/FileUploader/index.tsx
+++ src/components/molecules/FileUploader/index.tsx
@@ -1,11 +1,21 @@
 "use client";
 
 import { FC } from "react";
+import { DropzoneComponent } from "../DropzoneComponent";
 
 interface FileUploaderProps {}
 
 const FileUploader: FC<FileUploaderProps> = ({}) => {
-  return <div>File Uploader</div>;
+  return (
+    <div>
+      <DropzoneComponent
+        name="Dropzone"
+        onSubmit={() => console.log("FILES UPLOADED")}
+        content="Upload a question"
+        classes={{ wrapper: "border-0" }}
+      />
+    </div>
+  );
 };
 
 export { FileUploader };
