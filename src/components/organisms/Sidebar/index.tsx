aa4a62b800f7c87557ba39dfaae59c8c570caf84
Alex <oleksii.stupak@aleannlab.com>
2024-11-29 10:49:56 +0200

    fix icon

--- src/components/organisms/Sidebar/index.tsx
+++ src/components/organisms/Sidebar/index.tsx
@@ -59,15 +59,13 @@ const Sidebar = () => {
               //   className="cursor-pointer"
               // />
               <>
-                <div className="w-[137px] opacity-0 h-[60px] relative"> {/*//TODO/*/}
-                  {/* <div className="w-[110px] left-[38px] top-[10px] absolute text-[#547a91] text-base font-semibold leading-none">Career Buddy</div> */}
-                  <Icon type="DepartmentLogoFull" />
+                <div className="w-[169px] h-9 relative">
+                  <div className="w-[110px] left-[38px] top-[10px] absolute text-[#547a91] text-base font-semibold leading-none">Career Buddy</div>
+                  <Icon type="LogoIcon" />
                 </div>
               </>
             ) : (
-              <div className="hidden">
-              <Icon type="DepartmentLogo" />
-              </div>
+              <Icon type="LogoIcon" />
             )}
           </button>
           <Button onClick={handleToggle} variant="ghost">
