0fa3ca9d88f6edb14128a7cd2cce407759db720c
Alex <oleksii.stupak@aleannlab.com>
2025-03-11 16:53:14 +0200

    fix: CardAuthentication Description

--- src/components/organisms/Cards/CardAuthentication/index.tsx
+++ src/components/organisms/Cards/CardAuthentication/index.tsx
@@ -8,13 +8,18 @@ const CardAuthentication = () => {
     <CardTemplate
       classes={{
         cardTitle: "text-center",
+        card: "",
+        cardDescription: "text-center text-[#666]"
       }}
-      title="Welcome to Career Buddy!"
-      description="Log in or sign up to access personalized career guidance, upload study materials and images, and receive tailored responses to help you succeed in school and beyond."
+      title="Welcome to Espen!"
+      description="Log in or sign up for personalized AI tutoring, career guidance, homework help, exam strategies, and multilingual revision notes. Teachers also receive instant CAPS lesson plans, assessment creation and other intervention tools"
     >
-      <CardTemplate.Content className="flex flex-col gap-4 mt-4" >
+      <CardTemplate.Content className="flex flex-col gap-2 mt-4" >
         <Button variant="main" className="!rounded-md" size="xl" full href="/login" as="link">
           Log in
+        </Button>
+        <Button variant="outlineMain" className="!rounded-md" size="xl" full href="/sign-up" as="link">
+          Sign up
         </Button>
       </CardTemplate.Content>
       <CardTemplate.Footer className="flex flex-col w-full mt-6 justify-center items-center">
@@ -22,7 +27,7 @@ const CardAuthentication = () => {
             variant="link"
             size="xs"
             className="text-md-center rounded-md mx-auto  underline-offset-1 underline font-normal self-end"
-            href="/reset-password"
+            href="/authentication"
             as="link"
           >
             Stay logged out
