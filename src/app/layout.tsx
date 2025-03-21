ac28b19542ae61bb1a2e8e9968580dfffd258dbf
Alex <oleksii.stupak@aleannlab.com>
2025-03-21 11:37:26 +0200

    fix: favicon.ico

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,21 +31,25 @@ export default function RootLayout({
         <link
           rel="apple-touch-icon"
           sizes="180x180"
-          href="/apple-touch-icon.png"
+          href="/favicon.ico?v=2"
         />
         <link
           rel="icon"
           type="image/png"
           sizes="32x32"
-          href="/favicon-32x32.png"
+          href="/favicon.ico?v=2"
         />
         <link
           rel="icon"
           type="image/png"
           sizes="16x16"
-          href="/favicon-16x16.png"
+          href="/favicon.ico?v=2"
         />
-        <link rel="manifest" href="/site.webmanifest" />
+        <link
+          rel="icon"
+          href="/favicon.ico?v=2"
+        />
+        <link rel="manifest" href="/site.webmanifest?v=2" />
         <meta name="msapplication-TileColor" content="#da532c" />
         <meta name="theme-color" content="#ffffff" />
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"></meta>
