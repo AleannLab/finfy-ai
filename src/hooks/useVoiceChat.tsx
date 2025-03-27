956dab5a649de5b19fbdbf569ad7b816da9271d3
Alex <oleksii.stupak@aleannlab.com>
2025-03-27 11:45:18 +0200

    feat: changed voice to ash

--- src/hooks/useVoiceChat.tsx
+++ src/hooks/useVoiceChat.tsx
@@ -71,7 +71,7 @@ const useVoiceChat = (instructionsForAssistant?: string) => {
 
     client.updateSession({
       instructions: instructionsForAssistant ? instructionsForAssistant : instructions,
-      voice: "alloy",
+      voice: "ash",
       input_audio_transcription: { model: "whisper-1" },
       turn_detection: { type: "server_vad" },
     });
